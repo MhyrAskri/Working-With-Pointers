53e4178a58b24dc80711e6f89663f477fad2815a
MhyrAskri <askari.m1379@gmail.com>
2021-09-16 19:23:03 +0430

    pointers

---
+++ cmake-build-debug/CMakeFiles/WorkingWithPointers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WorkingWithPointers.dir/main.cpp.obj"
+  "WorkingWithPointers.exe"
+  "WorkingWithPointers.exe.manifest"
+  "WorkingWithPointers.pdb"
+  "libWorkingWithPointers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WorkingWithPointers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
